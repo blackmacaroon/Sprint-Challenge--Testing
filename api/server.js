43b4c6d6587c5e7108441169bb2280b4604b5661
Kayla <6lackmacaroon@gmail.com>
2019-06-21 11:03:27 -0600

    get list tests passing - one post test passing

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('../games/games-model.js');
 
 const server = express();
 
@@ -11,21 +12,21 @@ server.get('/', (req, res) => {
       })
       .get('/games', async (req, res) => {
             try {
-                  
+                  const games = await db.get('games')
+                  res.status(200).json(games)
             } catch (error) {
                   console.log(error);
                   res.status(500).json(error)
             }
       })
       .post('/games', async (req, res) => {
-            const game = {...req.body,
-                  
-            };
+            const game = req.body
             if(!game.title && !game.genre){
                   return res.status(422).json({message: 'sorry, title and genre are required'});  
             } 
             try {
-
+                  const newGame = await db.add(game)
+                  res.status(201).json({ message: 'good game.'})
             } catch (error) {
                   console.log(error);
                   res.status(500).json({ error: 'sorry, we failed adding that to the database'});
