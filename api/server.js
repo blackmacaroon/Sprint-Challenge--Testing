6d52ef474cf7790cd296e484699cfbe8599677a4
Kayla <6lackmacaroon@gmail.com>
2019-06-21 09:56:35 -0600

    sanity route server tests built - debugging test err - jest: command not found

--- api/server.js
+++ api/server.js
@@ -7,6 +7,29 @@ server.use(express.json());
 //sanity check 
 server.get('/', (req, res) => {
       res.status(200).json({ message: "oh, hey!"})
-})
+      
+      })
+      .get('/games', async (req, res) => {
+            try {
+                  
+            } catch (error) {
+                  console.log(error);
+                  res.status(500).json(error)
+            }
+      })
+      .post('/games', async (req, res) => {
+            const game = req.body;
+            if(game.title && game.genre){
+                  try {
+
+                  } catch (error){
+                        res.status(500).json(error)
+                  }     
+            } else {
+                  res.status(400).json({ message: 'title and genre are required'})
+            }
+            
+      })
+
 
 module.exports = server;
---
+++ api/server.spec.js
@@ -0,0 +1,24 @@
+const server = require('./server.js');
+const supertest = require('supertest');
+
+describe('server', () => {
+      describe('GET "/" sanity check', () => {
+            it('should render with no errors and 200 ok', () => {
+                  return supertest(server)
+                  .get('/')
+                  .expect(200)
+            })
+            it('should respond with json format', () => {
+                  return supertest(server)
+                  .get('/')
+                  .expect('Content-Type', /json/i)
+            })
+            it('should respond with a message', async () => {
+                  await supertest(server)
+                  .get('/')
+                  .then(res => {
+                        expect(res.body).toContain({message})
+                  })
+            })
+      })
+})
